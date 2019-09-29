263e7c05d2f95dfd7bacd1ceb99854ba51436bf4
amercier8 <a.mercier8@gmail.com>
2019-09-29 18:23:39 +0200

    api get sessions for a single user

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\RunningSession;
+use App\Entity\User;
 use App\Repository\RunningSessionRepository;
 use App\Service\RunningSessionService;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -42,6 +43,33 @@ class ApiController
     public function getRunningSessions(RunningSessionRepository $runningSessionRepository, RunningSessionService $runningSessionService): JsonResponse
     {
         $runningSessions = $runningSessionRepository->findAll();
+
+        $arrayCollection = [];
+        foreach ($runningSessions as $runningSession) {
+            $arrayCollection[] = $runningSessionService->getRunningSessionsAsArray($runningSession);
+        }
+
+        return new JsonResponse($arrayCollection);
+    }
+
+    /**
+     * @Route("/api/users/{id}/runningsessions", name="get_running_sessions_by_user")
+     *
+     * @param User|null $user
+     * @param RunningSessionService $runningSessionService
+     *
+     * @return JsonResponse
+     */
+    public function getRunningSessionsByUser(?User $user, RunningSessionService $runningSessionService): JsonResponse
+    {
+        if ($user instanceof User === false) {
+            return new JsonResponse([
+                'code' => 404,
+                'message' => 'Running Session not found',
+            ]);
+        }
+
+        $runningSessions = $user->getRunningSessions();
 
         $arrayCollection = [];
         foreach ($runningSessions as $runningSession) {
